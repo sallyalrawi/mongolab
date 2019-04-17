9a0f210b5bd5097047b344103bef168bde3a294a
sally <sallyakrum83@gmail.com>
2019-04-16 19:22:22 -0500

    added some html

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-
+var path = require("path");
 // Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
@@ -27,9 +25,58 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 
 // Routes
+// ======
+
+// Simple index route
+app.get("/saved", function(req, res) {
+  res.sendFile(path.join(__dirname + "./public/saved.html"));
+});
+
+
+
+// A GET route for scraping the nytimes website
+// app.get("/scrape new article", function(req, res) {
+//   // First, we grab the body of the html with axios
+//   axios.get("https://www.nytimes.com/").then(function(response) {
+//     // Then, we load that into cheerio and save it to $ for a shorthand selector
+//     var $ = cheerio.load(response.data);
+
+//     // Now, we grab every h2 within an article tag, and do the following:
+//     $("article h2").each(function(i, element) {
+//       // Save an empty result object
+//       var result = {};
+
+//       // Add the text and href of every link, and save them as properties of the result object
+//       result.title = $(this)
+//         .children("a")
+//         .text();
+//       result.link = $(this)
+//         .children("a")
+//         .attr("href");
+
+//       // Create a new Article using the `result` object built from scraping
+//       db.Article.create(result)
+//         .then(function(dbArticle) {
+//           // View the added result in the console
+//           console.log(dbArticle);
+//         })
+//         .catch(function(err) {
+//           // If an error occurred, log it
+//           console.log(err);
+//         });
+//     });
+
+//     // Send a message to the client
+//     res.send("Scrape Complete");
+//   });
+// });
+
+// // Route for scrape a new Article from the db
+
 
 // Start the server
 app.listen(PORT, function() {
