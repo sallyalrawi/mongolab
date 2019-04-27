26c68c645f819afe74fca80b8f0e6613bd106b06
sally <sallyakrum83@gmail.com>
2019-04-27 12:54:14 -0500

    added some files

--- models/Note.js
+++ models/Note.js
@@ -1,17 +1,29 @@
+// Note model
+// ==========
 
-var mongoose = require('mongoose');
+// Require mongoose
+var mongoose = require("mongoose");
+// Create the schema class using mongoose's schema method
 var Schema = mongoose.Schema;
 
-var NoteSchema = new Schema({
-
- title: String,
- 
- body: String
+// Create the noteSchema with the schema object
+var noteSchema = new Schema({
+  // The headline is the article associate with the note
+  _headlineId: {
+    type: Schema.Types.ObjectId,
+    ref: "Headline"
+  },
+  // date is just a string
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  // as is the noteText
+  noteText: String
 });
 
+// Create the Note model using the noteSchema
+var Note = mongoose.model("Note", noteSchema);
 
-
-
-var Note = mongoose.model("Note", NoteSchema);
-
+// Export the Note model
 module.exports = Note;
